f8b42dea4209813152f8656a6d0396ae95c9d29b
Thai Nguyen Hung <hungthai1401.it@gmail.com>
2025-10-16 16:00:56 +0700

    refactor(nvim): comment out unused insert mode key mappings for clarity

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,12 +4,12 @@ if true then
   local msg = [[<cmd>echohl Error | echo "KEY DISABLED" | echohl None<CR>]]
 
   -- Disable arrow keys in insert mode with a styled message
-  vim.api.nvim_set_keymap("i", "<Up>", "<C-o>" .. msg, { noremap = true, silent = false })
-  vim.api.nvim_set_keymap("i", "<Down>", "<C-o>" .. msg, { noremap = true, silent = false })
-  vim.api.nvim_set_keymap("i", "<Left>", "<C-o>" .. msg, { noremap = true, silent = false })
-  vim.api.nvim_set_keymap("i", "<Right>", "<C-o>" .. msg, { noremap = true, silent = false })
-  vim.api.nvim_set_keymap("i", "<Del>", "<C-o>" .. msg, { noremap = true, silent = false })
-  vim.api.nvim_set_keymap("i", "<BS>", "<C-o>" .. msg, { noremap = true, silent = false })
+  -- vim.api.nvim_set_keymap("i", "<Up>", "<C-o>" .. msg, { noremap = true, silent = false })
+  -- vim.api.nvim_set_keymap("i", "<Down>", "<C-o>" .. msg, { noremap = true, silent = false })
+  -- vim.api.nvim_set_keymap("i", "<Left>", "<C-o>" .. msg, { noremap = true, silent = false })
+  -- vim.api.nvim_set_keymap("i", "<Right>", "<C-o>" .. msg, { noremap = true, silent = false })
+  -- vim.api.nvim_set_keymap("i", "<Del>", "<C-o>" .. msg, { noremap = true, silent = false })
+  -- vim.api.nvim_set_keymap("i", "<BS>", "<C-o>" .. msg, { noremap = true, silent = false })
 
   -- Disable arrow keys in normal mode with a styled message
   vim.api.nvim_set_keymap("n", "<Up>", msg, { noremap = true, silent = false })
