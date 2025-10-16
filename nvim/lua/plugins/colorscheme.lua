45dfa12207c17a03ae569b2b67ca72857c06b6f2
Thai Nguyen Hung <hungthai1401.it@gmail.com>
2025-10-16 16:25:12 +0700

    feat(colorscheme): add new colorscheme configuration for Vitesse

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  -- Use vitesse
+  {
+    "https://codeberg.org/pdewey/vitesse-nvim",
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "vitesse",
+    },
+  },
+}
